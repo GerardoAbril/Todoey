0a845e30863322d3c9616634abdd6a7970acbcbb
Gerardo Abril <gerardoabril@gerardos-macbook-pro.local>
2018-09-24 10:29:17 -0400

    Todo list complete with datasource and delegate methods

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Gerardo Abril on 9/24/18.
+//  Copyright © 2018 Gerardo Abril. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewListController: UITableViewController{
+
+    let itemArray = ["Find Mike", "Buy eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //Mark = TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    /*
+     This is how to initialize a table view so that the number of rows
+     is appropriate to how much the user has added
+    */
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //Mark - TableView Delegate Methods
+    
+    /*
+     This method demonstrates how to add an accessory so that a checkmark will appear
+     whenever the user selects a single cell
+    */
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print (itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+
+}
+
